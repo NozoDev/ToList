1b93ef52fa65dbe02b6833a622ca114dd502f7f3
Kevin rojas <kevinroj326@gmail.com>
2023-06-26 23:03:16 -0500

    agregue una funcionalidad para que  no me agregara un campo vacio, al hacer una nueva tarea

--- src/components/NewTodoView.vue
+++ src/components/NewTodoView.vue
@@ -3,17 +3,27 @@ import { todosFactory } from "../todosSetup";
 import router from "../router";
 
 const { add } = todosFactory();
-
 const addAndNavigate = (name) => {
+  if (name.trim().length === 0) {
+    // El campo está vacío, mostrar mensaje de error o realizar alguna acción
+    return;
+  }
   add(name);
   router.push("/");
+};
+
+let name = "";
+let isNameEmpty = true;
+
+const checkEmpty = () => {
+  isNameEmpty = name.trim().length === 0;
 };
 </script>
 
 <template>
   <div class="list-container">
     <label class="new-task">New Task</label>
-    <input class="new-task" type="text" v-model="name" />
+    <input class="new-task" type="text" v-model="name" @input="checkEmpty" />
     <span class="boton-add">
       <i
         class="bi bi-plus-circle-fill"
